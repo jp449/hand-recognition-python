6234fa4eebbc4c785b0812944929615cf6363466
jp449 <ceh3cg@virginia.edu>
2025-11-19 16:52:34 -0500

    copied code from https://www.geeksforgeeks.org/computer-vision/creating-a-finger-counter-using-computer-vision-and-opencv-in-python/

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from cvzone.HandTrackingModule import HandDetector
 
 mp_hands = mp.solutions.hands
 mp_draw = mp.solutions.drawing_utils
-hands = mp_hands.Hands(static_image_mode=True, max_num_hands=2, min_detection_confidence=0.3
+hands = mp_hands.Hands(static_image_mode=True, max_num_hands=2, min_detection_confidence=0.3)
 
 js = Javascript('''
     async function captureImage() {
